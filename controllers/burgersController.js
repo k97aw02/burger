a74b2a9964e882c1ee70e32c06c00dcd43e72847
k97aw02 <k97aw02@gmail.com>
2020-09-09 09:52:26 -0500

    add home path for heroku

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -4,6 +4,10 @@ var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
 var burger = require("../models/burger.js");
+//heroku home path
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "index.html"));
+});
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
