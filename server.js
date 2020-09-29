9c4dc35b6f11be6ba46acaddc841cef7aa8f448f
k97aw02 <k97aw02@gmail.com>
2020-09-28 21:31:25 -0500

    update JAWSDB

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.set("view engine", "handlebars");
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgersController.js");
 
+
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
